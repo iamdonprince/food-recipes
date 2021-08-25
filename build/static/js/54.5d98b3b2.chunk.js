f84df7069848db8038b948fdd6e9ca79a4e56c99
iamdonprince <princekumax@gmail.com>
2021-08-25 21:50:45 +0530

    update title

---
+++ build/static/js/54.5d98b3b2.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmyApp=this.webpackJsonpmyApp||[]).push([[54],{139:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return c}));var i=e(15),o=e(14),c=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,c=document.elementFromPoint(t/2,e/2);if(c){var r=c.closest("ion-content");r&&new Promise((function(n){return Object(o.c)(r,n)})).then((function(){Object(i.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=54.5d98b3b2.chunk.js.map
